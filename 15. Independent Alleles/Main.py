05b7b44cb80626f48caff82acc41064ec4440a2c
Yiran Shao <charlottesyr@outlook.com>
2020-05-04 00:53:56 -0400

    Uploaded Q 15

--- 15. Independent Alleles/Main.py
+++ 15. Independent Alleles/Main.py
@@ -5,3 +5,13 @@
 # Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree
 #         (don't count the Aa Bb mates at each level). Assume that Mendel's second law holds for the factors.
 
+# Binomial distribution with AaBb as success (P = 0.25)
+import math
+k = 5
+N = 8
+P = 2**k
+prob = 0
+for i in range(N, P+1):
+    newProb = (math.factorial(P))/(math.factorial(i)*(math.factorial(P-i)))*(0.25**i)*(0.75**(P-i))
+    prob += newProb
+print(round(prob,3))
